1bd55a7e61679a89e203ac9c6afcbfa0bec7d7bd
justweb8 <138186775+justweb8@users.noreply.github.com>
2026-09-19 14:15:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - Configuración general de HospedaYa
 const CONFIG = {
-  SUPABASE_URL: "https://gcuicpitzbcwqxlloodm.supabase.co/rest/v1/", // Reemplaza con tu Project URL
+  SUPABASE_URL: "https://gcuicpitzbcwqxlloodm.supabase.co", // Reemplaza con tu Project URL
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdWljcGl0emJjd3F4bGxvb2RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMzU4NTQsImV4cCI6MjA4NzkxMTg1NH0.NArYzoJqW4wpAUAvmy6Y4Pf1GPgsK5Tz1tkhxrAnpG8",               // Reemplaza con tu anon public key
   WHATSAPP_SOPORTE: "51975561764",                 // Tu número para renovaciones (código país + número)
   NOMBRE_SAAS: "HospedaYa PMS"
